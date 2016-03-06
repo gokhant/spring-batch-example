14ac4cf5c52e522a44cdeb3d1bc90c693e3b28a3
Gokhan Tuna <gokhant@gmail.com>
2016-03-06 19:37:50 +0200

    Initial commit

---
+++ src/main/java/BatchConfiguration.java
@@ -0,0 +1,14 @@
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by gokhant on 06/03/2016.
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+    public BatchConfiguration() {
+
+    }
+}
