5eedd9273c4d4ce83ed5f4104caec5bea63707c9
gokhant <gokhant@gmail.com>
2016-03-20 16:34:32 +0200

    added writer

--- src/main/java/gokhan/java/spring/batch/common/BatchConfiguration.java
+++ src/main/java/gokhan/java/spring/batch/common/BatchConfiguration.java
@@ -2,6 +2,8 @@ package gokhan.java.spring.batch.common;
 
 import gokhan.java.spring.batch.model.Measurement;
 import gokhan.java.spring.batch.model.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
@@ -14,19 +16,24 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+    Logger logger = LoggerFactory.getLogger(BatchConfiguration.class);
 
     @Autowired
     private StepExecutionListener stepListener;
@@ -56,7 +63,9 @@ public class BatchConfiguration {
 
     @Bean
     public ItemWriter<Result> resultWriter() {
-        return new ResultWriter();
+        return resultWriter(
+                new FileSystemResource("results.csv")
+        );
     }
 
     @Bean
@@ -71,14 +80,13 @@ public class BatchConfiguration {
 
     @Bean
     public Step step1(ItemReader<Measurement> reader, ItemWriter<Result> writer, ItemProcessor<Measurement, Result> processor) {
-        Step step = stepFactory.get("FirstStep")
+        return stepFactory.get("FirstStep")
                 .<Measurement, Result> chunk(1)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
                 .listener(stepListener)
                 .build();
-        return step;
     }
 
     private FieldSetMapper<Measurement> fieldSetMapper() {
@@ -97,5 +105,18 @@ public class BatchConfiguration {
         lineMapper.setLineTokenizer(tokenizer());
         lineMapper.setFieldSetMapper(fieldSetMapper());
         return lineMapper;
+    }
+
+    private ItemWriter<Result> resultWriter(FileSystemResource fsr) {
+        FlatFileItemWriter<Result> writer = new FlatFileItemWriter<Result>();
+        writer.setResource(fsr);
+        writer.setShouldDeleteIfExists(true);
+        DelimitedLineAggregator<Result> lineAggregator = new DelimitedLineAggregator<Result>();
+        lineAggregator.setDelimiter(",");
+        BeanWrapperFieldExtractor<Result> fExtractor = new BeanWrapperFieldExtractor<Result>();
+        fExtractor.setNames(new String[] {"cell.cellId", "cell.latitude", "cell.longitude", "cell.azimuth", "cell.beamWidth", "counter.name", "counter.value"});
+        lineAggregator.setFieldExtractor(fExtractor);
+        writer.setLineAggregator(lineAggregator);
+        return writer;
     }
 }
